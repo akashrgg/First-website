93ed53d5054c70865ac141fb72b50116a47e8e84
Ugffdyb <akashzzh146@gmail.com>
2024-05-22 14:42:15 +0530

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 
-function change_task(){
-     document.getElementById("change").innerHTML = "I DO MY BEST";
+function sendEmail(){
+     Email.send({
+          Host : "smtp.gmail.com",
+          Username : "akashzzh146@gmail.com",
+          Password : "akash123",
+          To : 'akashqq146@gmail.com',
+          From : document.getElementById("email").value,
+          Subject : "New Contact Form Enquiry",
+          Body : "And this is the body"
+      }).then(
+        message => alert(message)
+      );
 }
